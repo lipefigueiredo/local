5d98fce97df1cdad29d84936b9b37939deec53ba
lipefigueiredo <felipe_grb@hotmail.com>
2015-02-13 10:06:47 -0200

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
  /*  CRIANDO CONEXAO  COM BANCO DE DADOS 
- PHP + MYSQL VERSAO 
+ PHP + MYSQL VERSAO 1.0
  */
 $bdservidor = '127.0.0.1' ;    // aqui vem seu servidor 
 $bdUsuario = 'root'  ;      // aqui vem o nome de usuario do seu banco
@@ -9,8 +9,8 @@ $dbBanco = 'dbphp'   ;        // aqui vem o nome do banco
 $conexao = mysqli_connect($bdservidor, $bdUsuario , $bdSenha , $dbBanco );  //aqui a variavel conexao recebe  servidor , usuario , senha , banco
    if ( mysqli_connect_errno($conexao)) {  // se conexao tiver erro
         echo " problemas para conectar no banco verifique os dados!"; // imprime na tela a mensagem de erro
-	   die();   //essa funçao mata a conexao .. 
+	   die();   
     }
 
     
-    // testando github no pc
+    
